9260259a3a90611cf88a5903dfdf39b066d98fd4
Eddie Ringle <eddie@eringle.net>
2013-12-15 20:47:29 -0500

    Generate version information from Git HEAD
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

---
+++ include/erbium_version.h
@@ -0,0 +1,12 @@
+#ifndef __included_erbium_version_h
+#define __included_erbium_version_h
+
+extern const char er_version_full[];
+
+extern const int er_version_major;
+extern const int er_version_minor;
+extern const int er_version_patch;
+
+extern const char er_version_extra[];
+
+#endif /* __included_erbium_version_h */
--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+#include "include/erbium_version.h"
+
 int main(void)
 {
-	printf("Hello World!\n");
+	printf("\n== Erbium v%d.%d.%d ==\n\n",
+		   er_version_major, er_version_minor, er_version_patch);
+	printf("Full version info: %s\n", er_version_full);
 	return 0;
 }
 
